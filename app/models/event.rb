024e9d8c9427b36a24000c260be02da1e7f95f91
Daniel Palacio <danpal@gmail.com>
2011-05-07 18:41:00 -0500

    Comment lines to try on heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event
   include Mongoid::Document
   
-  field :name, type: String
-  field :schedule, type: String
-  has_many :applications
+  #field :name, type: String
+  #field :schedule, type: String
+  #has_many :applications
    
 end
