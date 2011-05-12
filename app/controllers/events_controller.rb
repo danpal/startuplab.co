5ff6e58ea7c5b8e8869e4315babfe48aa5ccdc12
Daniel Palacio <danpal@gmail.com>
2011-05-11 22:48:55 -0500

    Fix bug that prevented event names to be changed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,21 +22,40 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-
+    
   end
 
   def edit
     @event = Event.find(params[:id])
   end
 
-  def update
-    @event = Event.find(params[:id])
-    if @event.update_attributes(params[:event])
-      redirect_to(@event, :notice => "Event updated succesfully")
-    else
-      render :action => "edit"
-    end
+  # Since :name is also a key to allow updating the name
+  # we need to check the params, if they try updating the name
+  # we need to create a new event, with a new name and delete the old one
 
+  def update
+    puts "*******ON UPDATE ************"
+    @event = Event.find(params[:id])
+
+    #check if user is trying to change the name
+    puts "#{@event.name} == @event.name #{params[:event][:name]} = params[:name]"
+    if (@event.name != params[:event][:name])
+      new_event = @event.dup
+      new_event.name = params[:event][:name]
+      if new_event.save!
+        redirect_to(new_event, :notice => "Event updated succesfully") 
+        @event.destroy
+      else
+        render :action => "edit"
+      end
+      
+    else
+      if @event.update_attributes!(params[:event])
+        redirect_to(@event, :notice => "Event updated succesfully")
+      else
+        render :action => "edit"
+      end
+    end
   end
 
   def destroy
